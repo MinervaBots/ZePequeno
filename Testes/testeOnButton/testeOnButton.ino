9b56331c3ec150e82672a9c92b35a01f84d94e30
MinervabotsUser <minervabots@poli.ufrj.br>
2017-06-30 15:32:49 -0300

    testOnButton_2 (papauloedit)

--- Testes/testeOnButton/testeOnButton.ino
+++ Testes/testeOnButton/testeOnButton.ino
@@ -1,16 +1,21 @@
 #define ON_BUTTON              5
 
 bool btn_state = false;
+int aux = 0;
 
 void setup() {
   pinMode(ON_BUTTON, INPUT_PULLUP);
-  Serial.begin(9600); 
+  Serial.begin(9600);
 }
 
 void verifyOnButton() {
+  bool newState = btn_state;
   if (digitalReadFast(ON_BUTTON) == LOW) {
-    delay(50);
-    btn_state = !btn_state;
+    newState = !btn_state;
+  }
+  if (newState != btn_state) {
+    btn_state = newState;
+    delay(500);
   }
 }
 
@@ -18,11 +23,20 @@ void verifyOnButton() {
 void loop() {
   verifyOnButton();
   if (btn_state) {
+    if (aux == 0) {
+      Serial.println("ligando");
+      delay(5000);
+      aux = 1;
+    }
     Serial.println("ligado");
   }
-  else {
-    Serial.println("desligado");
+  else if (aux != 0) {
+    aux = 0;
+    btn_state = false;
+    Serial.println("desligando");
+    delay(150);
   }
-  delay(50);
-
+  else{
+    Serial.println("desligando");
+  }
 }
