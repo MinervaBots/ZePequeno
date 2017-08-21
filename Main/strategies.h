5c4567b15ca01fdcb6c52c30ab2d82ca53b0da1b
igor98 <igorcsi@poli.ufrj.br>
2017-08-21 03:32:00 -0300

    Update strategies.h

--- Main/strategies.h
+++ Main/strategies.h
@@ -6,12 +6,49 @@
 #ifndef STRATEGIES_H
 #define STRATEGIES_H
 
-void radarSearch();
-void starSearch();
-void archSearch();
+void starSearch();          // xxx0
+void radarSearch();         // xxx1
+
+void nothing();             // 000x
+void backwardStart();       // 001x
+void archStartRight();      // 010x
+void archStartLeft();       // 011x
+void attackStartRight();    // 100x
+void attackStartLeft();     // 101x
+void spinStartLeft();       // 110x
+void spinStartRight();      // 111x
+
 void avoidEdge();
-void archStart(bool);
-void backwardStart();
+
+void verifyStartStrategy();
+void verifySearchStrategy();
+
+void (*searchStrategy)();
+void (*startStrategy)();
+
+void verifyStartStrategy() {
+  if (not strategyButton(1)) {
+    if (strategyButton(2)) {
+      startStrategy = backwardStart;
+    }
+  else {
+    if (not strategyButton(2)) {
+      startStrategy = archStart;
+    }
+    else {
+      startStrategy = surpriseAttackStart;
+    }
+  }
+}
+  
+void verifySearchStrategy() {
+  if strategyButton(1) {
+    searchStrategy = radarSearch;
+  }
+  else {
+    searchStrategy = starSearch;
+  }
+}
 
 void radarSearch() {
   if (anyIR) {
@@ -28,7 +65,8 @@ void radarSearch() {
 void starSearch() {
   if (anyIR) {
     int side = readIRs();
-    move(1, side);  
+    move(1, side);
+    lastToSee = side;
   }
   else {
     forward();
@@ -45,8 +83,13 @@ void avoidEdge() {
   }
 }
 
-void archStart(bool right) {
-  move(-1, 0.5 - (not right));
+void archStartRight() {
+  move(-1, 0.5);
+  delay(delayArchStart);
+}
+  
+void archStartLeft() {
+  move(-1, -0.5);
   delay(delayArchStart);
 }
 
@@ -54,5 +97,37 @@ void backwardStart() {
   backwards();
   delay(delayBackStart);
 }
+
+void spinStartLeft() {
+  lastToSee = -1;
+}
+  
+void spinStartLeft() {
+  lastToSee = 1;
+}
+
+void attackStartRight() {
+  forward();
+  delay(attackF1delay);
+  spin(0);
+  delay(attackS1delay);
+  forward();
+  delay(attackF2delay);
+  spin(0);
+  delay(attackS2delay);
+}
+  
+void attackStartLeft() {
+  forward();
+  delay(attackF1delay);
+  spin(1);
+  delay(attackS1delay);
+  forward();
+  delay(attackF2delay);
+  spin(1);
+  delay(attackS2delay);
+}
+  
+void nothing() {}
 
 #endif
