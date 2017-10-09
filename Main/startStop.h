62b4ced6a79c1db15c5127023d404f4c2bf6d561
igor98 <igorcsi@poli.ufrj.br>
2017-10-08 21:41:21 -0300

    Update and rename bluetooth.h to startStop.h

--- Main/startStop.h
+++ Main/startStop.h
@@ -2,18 +2,19 @@
 #include "constants.h"
 #include "motors.h"
 
-#ifndef BLUETOOTH_H
-#define BLUETOOTH_H
+#ifndef STARTSTOP_H
+#define STARTSTOP_H
 
-void waitButton() {
+void waitBluetooth() {
   bool ready = false;
   while (not ready) {
     if (bluetooth.available() > 0) {
       ready = (bluetooth.read() == 1);
+    }
   }
 }
 
-void verifyToStop() {
+void verifyToStopBluetooth() {
   if (bluetooth.available() > 0) {
     if bluetooth.read() == 0 {
       while(true) {}
@@ -21,5 +22,15 @@ void verifyToStop() {
   }
 }
 
+void waitButton() {
+    while (not digitalRead(button)) {}
+  }
+
+void verifyToStopButton() {
+  if (digitalRead(button)) {
+    while(true) {}
+  }
+}
+  
 
 #endif
