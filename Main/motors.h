464c1a4d7bb9682fa7eda0c5a1b2ee4dd714066b
ArthGz <32315739+ArthGz@users.noreply.github.com>
2018-03-21 16:01:22 -0300

    Update motors.h

--- Main/motors.h
+++ Main/motors.h
@@ -1,8 +1,9 @@
-#ifndef MOTORS_H
-#define MOTORS_H
+#ifndef MOTORS_H         // Caso MOTORS_H não esteja definido
+#define MOTORS_H         // Define MOTORS_H
 
 //float forwardSpeed = 0;
 
+//=====Declaração das funções
 void move(float);
 void move(float angular, int maxPwm, bool reverse = false);
 void stop();
@@ -11,4 +12,3 @@ void spin(bool);
 void forward();
 
 #endif
-
