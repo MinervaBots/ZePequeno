af37d9bffec6a825c2d983bf769949806d5d76f4
ArthGz <32315739+ArthGz@users.noreply.github.com>
2018-03-21 16:03:16 -0300

    Update startStop.h

--- Main/startStop.h
+++ Main/startStop.h
@@ -1,14 +1,16 @@
-#ifndef STARTSTOP_H
-#define STARTSTOP_H
+#ifndef STARTSTOP_H              // Caso STARTSTOP_H não esteja declarado
+#define STARTSTOP_H              // Define STARSTOP_H
 
 #include <SoftwareSerial.h>
 #include <Button.h>
 
+//=====Declaração das funções
 void waitBluetooth();
 void verifyToStopBluetooth();
 void waitButton();
 void verifyToStopButton();
 
+//=====Declaração dos objetos
 extern Button button1;
 extern SoftwareSerial bluetooth; // verify pins (TX,RX)
 
