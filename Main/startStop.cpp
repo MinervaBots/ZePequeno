82f68548e928510b5bfb9633c305f3852898b8b7
ArthGz <32315739+ArthGz@users.noreply.github.com>
2018-03-21 16:02:25 -0300

    Update startStop.cpp

--- Main/startStop.cpp
+++ Main/startStop.cpp
@@ -1,3 +1,4 @@
+//=====Inclusão das bibliotecas
 #include <Arduino.h>
 #include "startStop.h"
 #include "constants.h"
@@ -6,23 +7,7 @@
 Button button1(button);
 SoftwareSerial bluetooth(9, 10);
 
-void waitBluetooth() {
-  bool ready = false;
-  while (not ready) {
-    if (bluetooth.available() > 0) {
-      ready = (bluetooth.read() == 1);
-    }
-  }
-}
-
-void verifyToStopBluetooth() {
-  if (bluetooth.available() > 0) {
-    if (bluetooth.read() == 0) {
-      while(true) {}
-    }
-  }
-}
-
+//=====Início da função waitButton
 void waitButton() {
   while (1) {
     if (button1.pressed()) {
@@ -31,6 +16,7 @@ void waitButton() {
   }
   }
 
+//=====Início da função verifyToStopButton
 void verifyToStopButton() {
   if (button1.pressed()) {
     while(1){
