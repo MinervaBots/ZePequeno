ba2d6abe9de36b72624cd96628935c6d312339de
ArthGz <32315739+ArthGz@users.noreply.github.com>
2018-03-21 16:02:07 -0300

    Update sensors.h

--- Main/sensors.h
+++ Main/sensors.h
@@ -1,13 +1,13 @@
-#ifndef SENSORS_H
-#define SENSORS_H
+#ifndef SENSORS_H           // Caso o SENSOR_H não esteja definido
+#define SENSORS_H           // Define SENSORS_H
 
 
+//======Declaração das funções
 bool readIR(bool);
-//int readIRs();
 bool anyIR(int* side);
 bool readEdge(bool);
 bool anyEdge(int*);
 bool strategyButton(int);
 
-
-#endif
+                             
+#endif                      
