3735cef7d4984c1e95da1c544dfb689546cbe9a8
ArthGz <32315739+ArthGz@users.noreply.github.com>
2018-05-22 14:32:19 -0300

    Update strategies.h

--- Testes/ZezaoGarotao/main/strategies.h
+++ Testes/ZezaoGarotao/main/strategies.h
@@ -1,26 +1,8 @@
 #ifndef STRATEGIES_H
 #define STRATEGIES_H
 
-void starSearch();          // xxx1
 void radarSearch();         // xxx0
 
-void nothing();             // 000x
-void backwardStart();       // 001x
-void archStartRight();      // 010x
-void archStartLeft();       // 011x
-void attackStartRight();    // 100x
-void attackStartLeft();     // 101x
-void spinStartLeft();       // 110x
-void spinStartRight();      // 111x
-
-void avoidEdge();
-
-void verifyStartStrategy();
-void verifySearchStrategy();
-
 extern int lastToSee;
-extern void (*searchStrategy)();
-extern void (*startStrategy)();
 
 #endif
-
