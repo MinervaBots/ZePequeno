4d0adfba8fe415346429c2ee5e8f2baaf2031e35
ArthGz <32315739+ArthGz@users.noreply.github.com>
2018-05-22 14:31:40 -0300

    Update startStop.h

--- Testes/ZezaoGarotao/main/startStop.h
+++ Testes/ZezaoGarotao/main/startStop.h
@@ -1,18 +1,10 @@
 #ifndef STARTSTOP_H
 #define STARTSTOP_H
 
-#include <SoftwareSerial.h>
 #include <Button.h>
 
-void waitBluetooth();
-void verifyToStopBluetooth();
-void waitButton();
-void verifyToStopButton();
-void waitIR();
-void verifyToStopIR();
+void waitIR(bool);
+void verifyToStopIR(bool);
 
-extern Button button1;
-extern SoftwareSerial bluetooth; // verify pins (TX,RX)
-extern IRrecv irrecv(RECV_PIN);
 
 #endif
