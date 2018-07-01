390e697ab139f15fa31c38fc07ee475d5960ddc9
ArthGz <32315739+ArthGz@users.noreply.github.com>
2018-06-30 22:17:21 -0300

    Update constants.h

--- Main/constants.h
+++ Main/constants.h
@@ -1,58 +1,47 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
+#ifndef CONST_H
+#define CONST_H
 
-// Definição de todas as constantes usadas na programação
+#define maxPWM 180
+#define PWMAcceleration 255
+#define maxPWMForward 255
+#define maxStarPWM 120
+#define backPWM 100
+#define leftFlankPWM 255
+#define rightFlankPWM 255
 
-//#define convertPWM 0.31 // test
-#define maxPWM 255
-#define maxStarPWM 150//maxPWM * convertPWM 
-#define backPWM 150
+#define accelerationRate 0.0006   //0.0006
 
-#define RECV_PIN 11
+#define delayBackEdge 300
+#define delaySpinEdge 200
+#define leftFlankDelay 300
+#define rightFlankDelay 300
+#define edgeLimit 600
+#define leftDibreDelay 200
+#define rightDibre 200
 
-#define delayBackEdge 200//((int)((100.0 / maxPWM) * 300.0)) 
-#define delaySpinEdge 200//((int)((100.0 / maxPWM) * 200.0)) 
+#define leftFlankAngular 0.20
+#define rightFlankAngular 0.20
+#define leftDibreAngular 0.40
+#define rightDibreAngular 0.40
 
-#define accelerationRate 0.0006 // test
-#define edgeLimit 600 
-#define delayBackStart 140
-
-#define arcPWM 80
-#define delayArchStartRight 120
-#define delayBackLookingRight 190
-#define archAngleRight 0.45
-
-#define delayArchStartLeft 120
-#define delayBackLookingLeft 190
-#define archAngleLeft 0.45
-
-#define attackF1delayRight 190 
-#define attackF2delayRight 180 
-#define attackS1delayRight 110 
-#define attackS2delayRight 150 
-
-#define attackF1delayLeft 190 
-#define attackF2delayLeft 180 
-#define attackS1delayLeft 110 
-#define attackS2delayLeft 150
-
-#define led 0
+#define led 11
 #define leftWheelPWM 21
 #define rightWheelPWM 20
 #define leftWheelP1 18
 #define leftWheelP2 19
-#define rightWheelP1 15
-#define rightWheelP2 14
-#define leftIR 10
-#define rightIR 9
-#define button 5
-#define leftEdge 16
-#define rightEdge 17
+#define rightWheelP1 16
+#define rightWheelP2 17
+#define leftIR 9
+#define rightIR 0
+#define button 12
+#define rightEdge 23
+#define leftEdge 14
 
-#define SWITCH_ONE             4
-#define SWITCH_TWO             3
-#define SWITCH_THREE           2
-#define SWITCH_FOUR            1
+#define RECV_PIN 10
 
+#define SWITCH_ONE 6
+#define SWITCH_TWO 5
+#define SWITCH_THREE 2
+#define SWITCH_FOUR 1 
 
 #endif
