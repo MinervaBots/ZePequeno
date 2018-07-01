b08bf86b27d4396a26d4dbdc95017fd4c08ac3cf
ArthGz <32315739+ArthGz@users.noreply.github.com>
2018-06-30 22:19:54 -0300

    Update para a Winter 2018

--- Main/sensors.cpp
+++ Main/sensors.cpp
@@ -1,9 +1,47 @@
-//=====Inclusão das bibliotecas
 #include <Arduino.h>
 #include "sensors.h"
-#include "constants.h"
+#include "const.h"
 
-volatile unsigned long delayInterrupt;
+float lastIRreading = 0;
+float readIR()
+{
+  float reading = digitalReadFast(rightIR) + digitalReadFast(leftIR);
+  float ret = (1.0 - 0.8) * lastIRreading + reading * 0.8;
+  lastIRreading = reading;
+  return ret;
+}
+
+bool readIR(bool right)
+{
+  return right ? (digitalReadFast(rightIR)) : (digitalReadFast(leftIR));
+}
+
+bool anyIR(int* side)
+{
+  bool right = readIR(0);
+  bool left = readIR(1);
+  *side = right - left;
+  return (left || right);
+}
+
+bool readEdge(bool right)
+{
+  /*
+  Serial.print("Edge Direita: ");
+  Serial.print(analogRead(rightEdge));
+  Serial.print("   ||    Edge Esquerda: ");
+  Serial.println(analogRead(leftEdge));
+  */
+  return (right ? (analogRead(rightEdge) < edgeLimit) : (analogRead(leftEdge) < edgeLimit));
+}
+
+bool anyEdge(int *boardSide)
+{
+  bool right = readEdge(0);
+  bool left = readEdge(1);
+  *boardSide = right - left;
+  return (left || right);  
+}
 
 //=====Início da função strategyButton
 bool strategyButton(int selectionButton) {
@@ -19,64 +57,8 @@ bool strategyButton(int selectionButton) {
   {                                   // Caso botão 3 seja selecionado, vai retornar SWITCH_THREE
     return digitalRead(SWITCH_THREE);
   }
-  return digitalRead(SWITCH_FOUR);  
-                                      // Caso botão 4 seja selecionado, vai retornar SWITCH_FOUR
-}
-
-//=====Início da função readEdge
-bool readEdge(bool right) 
-{              
-  return (right ? (analogRead(rightEdge)  < edgeLimit) : analogRead(leftEdge)  < edgeLimit);
-  // Caso ele receba right como verdadeiro(1), vai fazer analogRead do sensor direito, retornando verdadeiro(1) caso esteja vendo a borda
-}
-
-//=====Início da função anyEdge
-bool anyEdge(int *boardSide) 
-{
-  bool right = readEdge(0);          // Lê o sensor de borda direito
-  bool left = readEdge(1);           // Lê o sensor de borda esquerdo
-  *boardSide = left - right;         // boardSide resulta em 0 caso os dois sensores estiverem vendo, ou não estiverem vendo algo.
-                                     // Caso um esteja e o outro não, boardSide será igual à 1
-  return (left || right);            // Retorna left ou right, tem o objetivo de retornar um valor verdadeiro(1), não importando qual seja
-                                     // Dessa forma, caso boardSide seja 0 e o valor retornado seja verdadeiro, está vendo algo, mesma coisa ao contrário
-}
-
-//=====Início da função readIR
-bool readIR(bool right) 
-{
-  return right ? (digitalReadFast(rightIR)) : (digitalReadFast(leftIR));  
-  // Caso ele receba right como verdadeiro(1), vai fazer analogRead do sensor direito, retornando verdadeiro(1) caso esteja vendo inimigo
-}
-
-//=====Início da função anyIR
-bool anyIR(int *side)
-{
-  bool right = readIR(0);             // Lê o sensor IR direito
-  bool left = readIR(1);              // Lê o sensor IR esquerdo
-  *side = right - left;               // side resulta em 0 caso os dois sensores estiverem vendo, ou não estiverem vendo algo
-                                      // Caso um esteja e outro não, side será igual a 1
-  return (left || right);             // Retorna left ou right, tem o objetivo de retornar um valor verdadeiro(1), não importando qual seja
-                                      // Dessa forma, caso side seja 0 e o valor returnado seja verdadeiro, está vendo algo, mesma coisa ao contrário
-}
-
-//=====Início da função edgeInterrupt
-void edgeInterrupt()
-{
-  move(0, backPWM, 1);
-  delayInterrupt = false;
-}
-
-// Usar interrupção para fazer estrategias de início
-
-//=====Início da função IRInterrupt
-void IRInterrupt()
-{
-  delayInterrupt = false;
-}
-
-void myDelay(unsigned long delayTime)
-{
-  unsigned long start = millis();
-  while((millis() - start < delayTime) and delayInterrupt);
-  delayInterrupt = true;
+  if (selectionButton == 4)
+  {
+    return digitalRead(SWITCH_FOUR);  
+  }                                   // Caso botão 4 seja selecionado, vai retornar SWITCH_FOUR
 }
